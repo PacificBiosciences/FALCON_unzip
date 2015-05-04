aa57ba511705e9d39e39b446047fa472e938a5c5
pb-jchin <jchin@pacificbiosciences.com>
2015-05-03 21:45:48 -0700

    add setup.py and the initial iteration of `fc_phasing.py`

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+from distutils.core import Extension
+
+import glob
+
+install_requires=[ "networkx >= 1.7" ]
+
+scripts = glob.glob("src/py_scripts/*.py")
+
+setup(name='falcon_kit_phasing',
+      version='0.3.0',
+      description='phasing module for Falcon.',
+      author='Jason Chin',
+      author_email='jchin@pacificbiosciences.com',
+      packages=['falcon_kit_phase'],
+      package_dir={'falcon_kit':'src/py/'},
+      scripts = scripts,
+      zip_safe = False,
+      install_requires=install_requires
+     )
+
