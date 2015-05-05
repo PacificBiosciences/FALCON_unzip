e60a89e5c3b43d7a6be11e40f609a3ddbc2828b4
pb-jchin <jchin@pacificbiosciences.com>
2015-05-05 11:01:26 -0700

    modify the script to be compitable to last few commit

--- examples/unzip_ctg_000000F.sh
+++ examples/unzip_ctg_000000F.sh
@@ -3,13 +3,13 @@ mkdir -p 3-falcon_unzip/000000F
 cd 3-falcon_unzip/000000F
 fc_fetch_reads.py --fofn ../../input.fofn --ctg_fa ../../2-asm-falcon/p_ctg.fa --ctg_id 000000F --ctg_map ../../2-asm-falcon/contig_to_read_map
 
-blasr 000000F_reads.fa 000000F_ref.fa  -noSplitSubreads -clipping subread  -placeRepeatsRandomly -minPctIdentity 70.0  -minMatch 12  -nproc 48 -bam -out aln.bam
+blasr 000000F_reads.fa 000000F_ref.fa  -noSplitSubreads -clipping subread  -placeRepeatsRandomly -minPctIdentity 70.0  -minMatch 12  -nproc 24 -bam -out aln.bam
 samtools sort aln.bam aln_sorted
 samtools index aln_sorted.bam
 rm aln.bam
 
-fc_phasing.py --bam aln_sorted.bam --fasta 000000F_ref.fa --ctg_id 000000F
-fc_phasing_readmap.py
+fc_phasing.py --bam aln_sorted.bam --fasta 000000F_ref.fa --ctg_id 000000F --base_dir ../
+fc_phasing_readmap.py --ctg_id 000000F --ctg_map ../../2-asm-falcon/contig_to_read_map --phased_reads phased_reads
 
 fc_ovlp_filter_with_phase.py --fofn ../../2-asm-falcon/las.fofn --max_diff 40 --max_cov 40 --min_cov 1 --n_core 12 --min_len 10000 --db ../../1-preads_ovl/preads.db  --rid_phase_map ./rid_to_phase > preads.p_ovl
 fc_phased_ovlp_to_graph.py preads.p_ovl --min_len 7000 > fc.log
