28506cb1ec282c3d2bc8042b046b575f83d4eca6
pb-jchin <jchin@pacificbiosciences.com>
2015-05-22 10:32:59 -0700

    1. using the whole assembly graph rather than the tiling path to identify the relationship between the reads and the contigs 2. only using the contained reads to track the relationship to reduce false hits

--- src/py_scripts/fc_track_reads.py
+++ src/py_scripts/fc_track_reads.py
@@ -3,6 +3,7 @@ from pypeflow.data import PypeLocalFile, makePypeLocalFile, fn
 from pypeflow.task import PypeTask, PypeThreadTaskBase, PypeTaskBase
 from pypeflow.controller import PypeWorkflow, PypeMPWorkflow, PypeThreadWorkflow
 from falcon_kit.FastaReader import FastaReader
+from falcon_kit.fc_asm_graph import AsmGraph
 import glob
 import sys
 import subprocess as sp
@@ -35,7 +36,7 @@ def dump_rawread_map(self):
                 if a_id != None and len(ovlp_data) > 0:
                     ovlp_data.sort()
                     out_ids = []
-                    for d in ovlp_data[:200]:
+                    for d in ovlp_data[:100]:
                         out_ids.append( d[1][1] ) #query id
                     print >>f, a_id, ovlp_count, " ".join(out_ids)
                 ovlp_count = 0
@@ -43,6 +44,8 @@ def dump_rawread_map(self):
                 ovlp_data = []
                 a_id = row[0]
 
+            if int(row[9]) > 500 or int(row[11]) - int(row[10]) > 500: #only output contained relationship
+                continue
             ovlp_len  = int(row[6]) - int(row[5])
             ovlp_data.append( (-ovlp_len, row) )
             ovlp_count += 1
@@ -51,7 +54,7 @@ def dump_rawread_map(self):
         if a_id != None and len(ovlp_data) > 0:
             ovlp_data.sort()
             out_ids = []
-            for d in ovlp_data[:200]:
+            for d in ovlp_data[:100]:
                 out_ids.append( d[1][1] ) #query id
             print >>f, row[0], ovlp_count, " ".join(out_ids)
 
@@ -76,6 +79,8 @@ def dump_pread_map(self):
                 ovlp_data = []
                 a_id = row[0]
 
+            if int(row[9]) > 5 or int(row[11]) - int(row[10]) > 5: #only output contained relationship
+                continue
             ovlp_len  = int(row[6]) - int(row[5])
             ovlp_data.append( (-ovlp_len, row) )
             ovlp_count += 1
@@ -159,11 +164,15 @@ wf = PypeMPWorkflow()
 
 p_ctg_tiling_path = makePypeLocalFile( os.path.join(asm_dir, "p_ctg_tiling_path") )
 a_ctg_tiling_path = makePypeLocalFile( os.path.join(asm_dir, "a_ctg_tiling_path") )
+sg_edges_list = makePypeLocalFile( os.path.join(asm_dir, "sg_edges_list") )
+utg_data = makePypeLocalFile( os.path.join(asm_dir, "utg_data") )
+ctg_paths = makePypeLocalFile( os.path.join(asm_dir, "ctg_paths") )
 
 inputs = { "rawread_id_file": rawread_id_file,
            "pread_id_file": pread_id_file,
-           "p_ctg_tiling_path": p_ctg_tiling_path,
-           "a_ctg_tiling_path": a_ctg_tiling_path }
+           "sg_edges_list": sg_edges_list,
+           "utg_data": utg_data,
+           "ctg_paths": ctg_paths }
 
 parameters = { "p_ovlp_files": [ fn(fobj) for fobj in all_p_ovlp_map_files.values() ],
                "r_ovlp_files": [ fn(fobj) for fobj in all_r_ovlp_map_files.values() ] }  # put these files as parameters to avoid tracking them
@@ -205,13 +214,21 @@ def gen_ctg_to_read_map(self):
 
     ctg_to_preads = {}
     rid_set = set()
+    asm_G = AsmGraph(fn(self.sg_edges_list), 
+                       fn(self.utg_data),
+                       fn(self.ctg_paths) )
 
-    for path_fn in tiling_files:
-        with open(path_fn) as f:
-            for row in f:
-                row = row.strip().split()
-                ctg = row[0]
-                frg0 = int(row[3])
+
+    with open(contig_to_read_map, "w") as f:
+        for ctg in asm_G.ctg_data:
+            rid_set = set()
+            ctg_to_preads = {}
+            if ctg[-1] == "R":
+                continue
+            ctg_g = asm_G.get_sg_for_ctg(ctg)
+            for n in ctg_g.nodes():
+                frg0 = int(n.split(":")[0])
+
                 ctg_to_preads.setdefault( ctg, set() )
 
                 rid = pread_did_to_rid[frg0].split("/")[1]
@@ -220,9 +237,9 @@ def gen_ctg_to_read_map(self):
                 ctg_to_preads[ctg].add((0, frg0, rid, oid))
                 rid_set.add(frg0)
 
-                for frg in list(overlap_pread.get(frg0,set())):
+                for frg in list(overlap_pread.get(frg0, set())):
                     rid = pread_did_to_rid[frg].split("/")[1]
-                    rid = int(rid[:-1])
+                    rid = int(int(rid) / 10)
                     if rid in rid_set:
                         continue
                     oid = rid_to_oid[rid]
@@ -230,23 +247,21 @@ def gen_ctg_to_read_map(self):
                     rid_set.add(rid)
 
 
-    for i in range(2,4):
-        for ctg in ctg_to_preads:
-            for r in list(ctg_to_preads[ctg]):
-                class_, pid, rid0, oid = r
+            for i in range(2,4):
+                for r in list(ctg_to_preads[ctg]):
+                    class_, pid, rid0, oid = r
 
-                for rid in list(overlap_rawread.get(rid0,set())):
-                    if rid in rid_set:
-                        continue
-                    oid = rid_to_oid[rid]
-                    ctg_to_preads[ctg].add((i ,rid0, rid, oid))
-                    rid_set.add(rid)
+                    for rid in list(overlap_rawread.get(rid0,set())):
+                        if rid in rid_set:
+                            continue
+                        oid = rid_to_oid[rid]
+                        ctg_to_preads[ctg].add((i ,rid0, rid, oid))
+                        rid_set.add(rid)
 
 
-    with open(contig_to_read_map, "w") as f:
-        for ctg in ctg_to_preads:
-            for r in list(ctg_to_preads[ctg]):
-                print >>f, ctg, r[0], "%09d" % r[1], "%09d" % r[2], r[3]
+            for ctg in ctg_to_preads:
+                for r in list(ctg_to_preads[ctg]):
+                    print >>f, ctg, r[0], "%09d" % r[1], "%09d" % r[2], r[3]
 
 wf.addTask( gen_ctg_to_read_map )
 wf.refreshTargets()
