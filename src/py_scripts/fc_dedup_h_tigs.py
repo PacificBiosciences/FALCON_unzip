b6468fb9a54b4fe589f948061b2d3b66c069ff6e
pb-jchin <jchin@pacificbiosciences.com>
2015-09-17 13:15:02 -0700

    reduce the amount of false negative of duplicated contig identification

--- src/py_scripts/fc_dedup_h_tigs.py
+++ src/py_scripts/fc_dedup_h_tigs.py
@@ -13,6 +13,7 @@ with open("hp_aln.coor") as f:
         if q_cov > 99 and idt > 99:
             filter_out.add(row[-1])
 
+
 h_ctg_to_phase = {}
 with open("h_ctg_path") as f:
     for row in f:
@@ -22,20 +23,19 @@ with open("h_ctg_path") as f:
         h_ctg_to_phase[row[0]].setdefault( ( b_id, ph_is ), 0)
         h_ctg_to_phase[row[0]][ ( b_id, ph_is ) ] += 1
 
-
-
+h_ids = open("h_ctg_ids","w")
 with open("h_ctg.fa", "w") as f:
     h_tig_all = FastaReader("h_ctg_all.fa")
     for r in h_tig_all:
-
-
-
         if r.name in filter_out:
             edge_count = sum(h_ctg_to_phase[ r.name ].values())
-            unphased_edge_count = h_ctg_to_phase[ r.name ] .get( (-1, 0) )
+            unphased_edge_count = h_ctg_to_phase[ r.name ] .get( (-1, 0), 0 )
+
             print r.name, edge_count, unphased_edge_count
-            if unphased_edge_count == edge_count:
+            if edge_count - unphased_edge_count < 5:
                 continue
 
         print >>f, ">"+r.name
         print >>f, r.sequence
+        print >> h_ids, r.name
+h_ids.close()
