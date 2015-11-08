d72b95b053d5b870aabec172b1317277ff52c2e9
pb-jchin <jchin@pacificbiosciences.com>
2015-11-08 12:33:46 -0800

    make read_tracker as a module rename the package to falcon_unzip

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ install_requires=[ "networkx >= 1.7" ]
 
 scripts = glob.glob("src/py_scripts/*.py")
 
-setup(name='falcon_kit_phasing',
+setup(name='falcon_unzip',
       version='0.1.0',
-      description='phasing module for Falcon.',
+      description='Falcon unzip',
       author='Jason Chin',
       author_email='jchin@pacificbiosciences.com',
-      packages=['falcon_kit_phasing'],
-      package_dir={'falcon_kit_phasing':'src/py/'},
+      packages=['falcon_unzip'],
+      package_dir={'falcon_unzip':'src/py/'},
       scripts = scripts,
       zip_safe = False,
       install_requires=install_requires
