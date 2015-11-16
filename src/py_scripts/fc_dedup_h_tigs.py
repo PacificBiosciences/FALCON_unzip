4e07b01cf3a942ff08395b53849902f1cb73d795
pb-jchin <jchin@pacificbiosciences.com>
2015-11-15 22:11:29 -0800

    if a draft haplotig is highly identical the primary contig and has the same phase assignment as the primary contig, we consider the draft haplotig is a duplicte. this reduce the 'holes' in the alignments on the primary for quiver consensus

--- src/py_scripts/fc_dedup_h_tigs.py
+++ src/py_scripts/fc_dedup_h_tigs.py
@@ -13,26 +13,42 @@ with open("hp_aln.coor") as f:
         if q_cov > 99 and idt > 99:
             filter_out.add(row[-1])
 
+p_ctg_to_phase = {}
+with open("p_ctg_path") as f:
+    for row in f:
+        row = row.strip().split()
+        b_id, ph_id = (int(row[-2]), int(row[-1]) )
+        p_ctg_to_phase.setdefault( row[0], {} )
+        p_ctg_to_phase[row[0]].setdefault( ( b_id, ph_id ), 0)
+        p_ctg_to_phase[row[0]][ ( b_id, ph_id ) ] += 1
+
 
 h_ctg_to_phase = {}
 with open("h_ctg_path") as f:
     for row in f:
         row = row.strip().split()
-        b_id, ph_is = (int(row[-2]), int(row[-1]) )
+        b_id, ph_id = (int(row[-2]), int(row[-1]) )
         h_ctg_to_phase.setdefault( row[0], {} )
-        h_ctg_to_phase[row[0]].setdefault( ( b_id, ph_is ), 0)
-        h_ctg_to_phase[row[0]][ ( b_id, ph_is ) ] += 1
+        h_ctg_to_phase[row[0]].setdefault( ( b_id, ph_id ), 0)
+        h_ctg_to_phase[row[0]][ ( b_id, ph_id ) ] += 1
 
 h_ids = open("h_ctg_ids","w")
 with open("h_ctg.fa", "w") as f:
     h_tig_all = FastaReader("h_ctg_all.fa")
     for r in h_tig_all:
+        p_ctg_phase = p_ctg_to_phase.get(r.name.split("_")[0], {})
+
         if r.name in filter_out:
             edge_count = sum(h_ctg_to_phase[ r.name ].values())
+            same_phase_to_p_ctg_count = 0
+            for  b_id, ph_id in h_ctg_to_phase[ r.name ]:
+                if b_id != -1:
+                    if (b_id, ph_id) in p_ctg_phase:
+                        same_phase_to_p_ctg_count += h_ctg_to_phase[ r.name ][ (b_id, ph_id) ]
             unphased_edge_count = h_ctg_to_phase[ r.name ] .get( (-1, 0), 0 )
 
-            print r.name, edge_count, unphased_edge_count
-            if edge_count - unphased_edge_count < 5:
+            print r.name, edge_count, unphased_edge_count, same_phase_to_p_ctg_count
+            if edge_count - unphased_edge_count - same_phase_to_p_ctg_count < 5: # there are many non-p_ctg phase segment, do not filter out
                 continue
 
         print >>f, ">"+r.name
