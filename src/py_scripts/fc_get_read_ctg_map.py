f818f845ff4635f40730e80f846b4526037b9f13
pb-jchin <jchin@pacificbiosciences.com>
2016-05-21 18:19:54 -0700

    new read to contig tracker code

---
+++ src/py_scripts/fc_get_read_ctg_map.py
@@ -0,0 +1,3 @@
+from falcon_unzip.get_read_ctg_map import get_read_ctg_map
+if __name__ == "__main__":
+    get_read_ctg_map()
---
+++ src/py_scripts/fc_rr_ctg_track.py
@@ -0,0 +1,4 @@
+from falcon_unzip.rr_ctg_track import main
+import sys
+if __name__ == "__main__":
+    main(sys.argv)
